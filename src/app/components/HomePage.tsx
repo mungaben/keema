7a80b68f9d5592363a00eeb5a671a40dbf8e2827
mungaben <kamaumungai3742@gmail.com>
2023-10-16 03:16:09 +0300

    style:h-mx

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -5,7 +5,7 @@ import Banner2 from "./Banner/Banner2";
 const HomePage = () => {
   return (
     <div className=" bg-[#016A70]/90 brightness-150 h-screen w-screen overflow-hidden">
-      <div className=" mt-[300px] space-x-[60px] md:mx-[450px]  flex  justify-between items-center flex-col md:flex-row">
+      <div className=" mt-[16dvw] space-x-[5dvw] md:mx-[20dvw]  flex  justify-between items-center flex-col md:flex-row">
         <section className="  items-center flex justify-center md:w-1/2">
           <Banner />
         </section>
